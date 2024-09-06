c2682a4c84649489f136b47dd8dbe1a7415a706f
hyerim <hyerimiya1216@gmail.com>
2024-09-06 14:24:01 +0900

    Feat : TypeScript 적용 및 DefaultTheme 적용

---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'styled-components';
+import 'styled-components';
+
+export const lightTheme: DefaultTheme = {
+  textColor: '#333',
+  bgColor: '#fff',
+};
+
+export const darkTheme: DefaultTheme = {
+  textColor: '#fff',
+  bgColor: '#333',
+};
