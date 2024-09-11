cdbc781cba95dfdd67e5e53f77a63d40944272a1
hyerim <hyerimiya1216@gmail.com>
2024-09-11 16:03:30 +0900

    Feat : [#5] data fetch, loading spinner 작업 추가

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,8 +1,11 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import ClipLoader from 'react-spinners/ClipLoader';
 
 const Container = styled.div`
-  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
 `;
 
 const Header = styled.header`
@@ -50,50 +53,57 @@ const Coin = styled.li`
   }
 `;
 
-const coins = [
-  {
-    id: 'btc-bitcoin',
-    name: 'Bitcoin',
-    symbol: 'BTC',
-    rank: 1,
-    is_new: false,
-    is_active: true,
-    type: 'coin',
-  },
-  {
-    id: 'eth-ethereum',
-    name: 'Ethereum',
-    symbol: 'ETH',
-    rank: 2,
-    is_new: false,
-    is_active: true,
-    type: 'coin',
-  },
-  {
-    id: 'hex-hex',
-    name: 'HEX',
-    symbol: 'HEX',
-    rank: 3,
-    is_new: false,
-    is_active: true,
-    type: 'token',
-  },
-];
+const Loader = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50vh;
+`;
+
+interface ICoins {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
 
 export default function Coins() {
+  const [coins, setCoins] = useState<ICoins[]>([]);
+  const [load, setLoad] = useState(true);
+
+  // Data Fetch: 100개 데이터 가져오기
+  useEffect(() => {
+    (async () => {
+      const response = await fetch('https://api.coinpaprika.com/v1/coins');
+      const json = await response.json();
+      const data = json.slice(0, 100);
+      setCoins(data);
+      setLoad(false);
+    })();
+  }, []);
+
   return (
     <Container>
       <Header>
         <Title>🪙 Coins 🪙</Title>
       </Header>
-      <CoinsList>
-        {coins.map((coin) => (
-          <Coin key={coin.id}>
-            <Link to={`/:${coin.id}`}>{coin.name}</Link>
-            <span>👉</span>
-          </Coin>
-        ))}
-      </CoinsList>
+      {load ? (
+        <Loader>
+          <ClipLoader color='#fdcb6e' loading={load} size={50} />
+        </Loader>
+      ) : (
+        <CoinsList>
+          {coins.map((coin) => (
+            <Coin key={coin.id}>
+              <Link to={`/:${coin.id}`}>{coin.name}</Link>
+              <span>👉</span>
+            </Coin>
+          ))}
+        </CoinsList>
+      )}
     </Container>
   );
 }
